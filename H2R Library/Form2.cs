81f0c307ff66cde7c27f25bf2c6f1ca13a5f47fd
hrushi10 <68341096+hrushi10@users.noreply.github.com>
2020-09-22 17:21:54 -0400

    gbg

--- H2R Library/Form2.cs
+++ H2R Library/Form2.cs
@@ -6,21 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using MySql.Data.MySqlClient;
-namespace H2R_Library
-{
-    public partial class Form2 : Form
-    {
-        public Form2()
-        {
-            InitializeComponent();
-        }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            runQuery();
-        }
 
        
     }
